1d0392e74cf442904513696df3a161f183590261
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-07-18 08:23:46 +0100

    added a subnet resource

--- 01-aws/main.tf
+++ 01-aws/main.tf
@@ -7,12 +7,29 @@ terraform {
     }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-    region = "us-east-1"
+    region = "eu-central-1"
     access_key = ""
     secret_key = ""
 }
 
+# Create a VPC
 resource "aws_vpc" "main" {
     cidr_block = "10.00.0.0/16"
+
+    tags = {
+        Name = "Main VPC"
+    }
+}
+
+# Create a subnet
+resource "aws_subnet" "main" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.0.0/24"
+    availability_zone = "eu-central-1a"
+
+    tags = {
+        Name = "Main Subnet"
+    }
 }
