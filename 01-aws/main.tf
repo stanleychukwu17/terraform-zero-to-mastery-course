e07323ccd7e011c7c8e759e778457bb4c9e9e44a
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-07-18 16:32:10 +0100

    added an internet gateway resource and a default route table resource

--- 01-aws/main.tf
+++ 01-aws/main.tf
@@ -24,12 +24,34 @@ resource "aws_vpc" "main" {
 }
 
 # Create a subnet
-resource "aws_subnet" "main" {
+resource "aws_subnet" "web" {
     vpc_id = aws_vpc.main.id
     cidr_block = var.web_subnet_cidr_block
     availability_zone = var.web_subnet_zone
 
     tags = {
-        Name = "Main Subnet"
+        Name = "Main web Subnet"
+    }
+}
+
+# Create an inernet gateway for the main vpc
+resource "aws_internet_gateway" "my_web_igw" {
+    vpc_id = aws_vpc.main.id
+    tags = {
+        "Name" = "${var.main_vpc_name} IGW"
+    }
+}
+
+# Create a default route table for the main vpc
+resource "aws_default_route_table" "main_default_rt" {
+    default_route_table_id = aws_vpc.main.default_route_table_id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.my_web_igw.id
+    }
+
+    tags = {
+        "Name" = "${var.main_vpc_name} default route table"
     }
 }
