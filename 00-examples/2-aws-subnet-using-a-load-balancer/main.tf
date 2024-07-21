47aa2c09fee0f463798f35d1d8fe1e2e3a0615e0
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-07-21 17:33:21 +0100

    corrected the output values

--- 00-examples/2-aws-subnet-using-a-load-balancer/main.tf
+++ 00-examples/2-aws-subnet-using-a-load-balancer/main.tf
@@ -176,15 +176,15 @@ output "vpc_id" {
 }
 
 output "subnet_id" {
-  value = aws_subnet.main.id
+  value = aws_subnet.web.id
 }
 
 output "internet_gateway_id" {
-  value = aws_internet_gateway.main.id
+  value = aws_internet_gateway.web_igw.id
 }
 
 output "route_table_id" {
-  value = aws_route_table.main.id
+  value = aws_route_table.web_rt.id
 }
 
 output "load_balancer_dns" {
