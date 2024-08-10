8636cc6d4f06faf0e7f9383a771d0fda609038a1
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-08-10 11:10:37 +0100

    ran terraform fmt - formatted the code to terraform standard

--- 01-aws/main.tf
+++ 01-aws/main.tf
@@ -1,57 +1,58 @@
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-    region = "eu-central-1"
-    access_key = ""
-    secret_key = ""
+  region     = "eu-central-1"
+  access_key = ""
+  secret_key = ""
 }
 
 # Create a VPC
 resource "aws_vpc" "main" {
-    cidr_block = var.vpc_cidr_block
+  cidr_block = var.vpc_cidr_block
 
-    tags = {
-        Name = "Dev Env ${var.main_vpc_name}"
-    }
+  tags = {
+    Name = "Dev Env ${var.main_vpc_name}"
+  }
 }
 
 # Create a subnet
 resource "aws_subnet" "web" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.web_subnet_cidr_block
-    availability_zone = var.web_subnet_zone
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.web_subnet_cidr_block
+  availability_zone = var.web_subnet_zone
 
-    tags = {
-        Name = "Main web Subnet"
-    }
+  tags = {
+    Name = "Main web Subnet"
+  }
 }
 
 # Create an inernet gateway for the main vpc
 resource "aws_internet_gateway" "my_web_igw" {
-    vpc_id = aws_vpc.main.id
-    tags = {
-        "Name" = "${var.main_vpc_name} IGW"
-    }
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    "Name" = "${var.main_vpc_name} IGW"
+  }
 }
 
 # Create a default route table for the main vpc
 resource "aws_default_route_table" "main_default_rt" {
-    default_route_table_id = aws_vpc.main.default_route_table_id
+  default_route_table_id = aws_vpc.main.default_route_table_id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.my_web_igw.id
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_web_igw.id
+  }
 
-    tags = {
-        "Name" = "${var.main_vpc_name} default route table"
-    }
+  tags = {
+    "Name" = "${var.main_vpc_name} default route table"
+  }
 }
