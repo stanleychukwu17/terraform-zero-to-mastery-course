0e6b589d20656fcab4066d9963993df166c72338
Chukwu Stanley <stanleychukwu17@yahoo.com>
2024-08-10 11:11:42 +0100

    ran the command terraform fmt, formated the code to terraform standard

--- 01-aws/variables.tf
+++ 01-aws/variables.tf
@@ -1,19 +1,19 @@
 variable "vpc_cidr_block" {
-    default = "10.0.0.0/16"
-    description = "The CIDR block for the VPC"
-    type = string
+  default     = "10.0.0.0/16"
+  description = "The CIDR block for the VPC"
+  type        = string
 }
 
 variable "main_vpc_name" {
-  
+
 }
 
 variable "web_subnet_cidr_block" {
-    default = "10.0.10.0/16"
-    description = "The CIDR block for the web subnet"
-    type = string
+  default     = "10.0.10.0/16"
+  description = "The CIDR block for the web subnet"
+  type        = string
 }
 
 variable "web_subnet_zone" {
-  
+
 }
